89aa1b1a678b05093c861ae2890884dd2ec1cb47
elvisimprsntr <casual@alecdawson.com>
2012-10-25 19:29:21 -0300

    Update lib/siriproxy-redeye.rb

--- lib/siriproxy-redeye.rb
+++ lib/siriproxy-redeye.rb
@@ -1,3 +1,7 @@
+require 'uri'
+require 'cora'
+require 'httparty'
+require 'rubygems'
 require 'siri_objects'
 
 class SiriProxy::Plugin::RedEye < SiriProxy::Plugin
@@ -9,6 +13,75 @@ class SiriProxy::Plugin::RedEye < SiriProxy::Plugin
     self.reip2 = config["reip2"]
   end
 
-# Under development
+  class Rest
+    include HTTParty
+    format :xml
+  end
+
+reurl1 = "#{reip1}:8080/redeye/rooms/0/devices/2/commands/send?commandId="
+reurl2 = "#{reip2}:8080/redeye/rooms/0/devices/2/commands/send?commandId="
+
+cmdzero 	= "3"
+cmdone 		= "4"
+cmdtwo 		= "5"
+cmdthree 	= "6"
+cmdfour 	= "7"
+cmdfive 	= "8"
+cmdsix 		= "9"
+cmdseven 	= "10"
+cmdeight 	= "11"
+cmdnine 	= "12"
+cmdchup		= "18"
+cmdchdn 	= "19"
+cmdenter 	= "14"
+cmdinfo 	= "13"
+codling 	= "21"
+cmdlast 	= "15"
+cmdmute 	= "17"
+cmdvolup 	= "16"
+cmdvoldn 	= "17"
+
+
+
+listen_for(/CNBC/i) {cnbc}
+listen_for(/ESPN/i) {espn}
+listen_for(/weather channel/i) {twc}
+listen_for(/nickelodeon/i) {nick}
+
+
+  def cnbc
+    say "OK. Changing the channel to CNBC."
+    Rest.get("#{reurl2)#{cmdfour}")
+    Rest.get("#{reurl2)#{cmdthree}")
+    Rest.get("#{reurl2)#{cmdenter}")
+    request_completed
+  end
+
+  def espn
+    say "OK. Changing the channel to ESPN."
+    Rest.get("#{reurl2)#{cmdthree}")
+    Rest.get("#{reurl2)#{cmdthree}")
+    Rest.get("#{reurl2)#{cmdenter}")
+    request_completed
+  end
+
+  def twc
+    say "OK. Changing the channel to The Weather Channel."
+    Rest.get("#{reurl2)#{cmdfive}")
+    Rest.get("#{reurl2)#{cmdeight}")
+    Rest.get("#{reurl2)#{cmdenter}")
+    request_completed
+  end
+
+  def nick
+    say "OK. Changing the channel to Nickelodeon."
+    Rest.get("#{reurl2)#{cmdsix}")
+    Rest.get("#{reurl2)#{cmdone}")
+    Rest.get("#{reurl2)#{cmdenter}")
+    request_completed
+  end
+
+
+
 	
 end
