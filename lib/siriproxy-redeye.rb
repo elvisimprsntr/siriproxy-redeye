0b7d32d81c61aff93af03bd593cad398ec561aa1
elvisimprsntr <casual@alecdawson.com>
2012-10-25 21:33:39 -0300

    Update lib/siriproxy-redeye.rb

--- lib/siriproxy-redeye.rb
+++ lib/siriproxy-redeye.rb
@@ -5,12 +5,35 @@ require 'rubygems'
 require 'siri_objects'
 
 class SiriProxy::Plugin::RedEye < SiriProxy::Plugin
-  attr_accessor :reipl1
-  attr_accessor :reipl2
+  attr_accessor :reip1
+  attr_accessor :reip2
   
   def initialize(config)
     self.reip1 = config["reip1"]
     self.reip2 = config["reip2"]
+    @reUrl1 = "#{self.reip1}:8080/redeye/rooms/0/devices/2/commands/send?commandId="
+    @reUrl2 = "#{self.reip2}:8080/redeye/rooms/0/devices/2/commands/send?commandId="
+
+	@cmdZero 	= 3
+	@cmdOne 	= 4
+	@cmdTwo 	= 5
+	@cmdThree 	= 6
+	@cmdFour 	= 7
+	@cmdFive 	= 8
+	@cmdSix 	= 9
+	@cmdSeven 	= 10
+	@cmdEight 	= 11
+	@cmdNine 	= 12
+	@cmdChup	= 18
+	@cmdChdn 	= 19
+	@cmdEnter 	= 14
+	@cmdInfo 	= 13
+	@cmdLang 	= 21
+	@cmdLast 	= 15
+	@cmdMute 	= 17
+	@cmdVolup 	= 16
+	@cmdVoldn 	= 17
+
   end
 
   class Rest
@@ -18,66 +41,80 @@ class SiriProxy::Plugin::RedEye < SiriProxy::Plugin
     format :xml
   end
 
-reurl1 = "#{self.reip1}:8080/redeye/rooms/0/devices/2/commands/send?commandId="
-reurl2 = "#{self.reip2}:8080/redeye/rooms/0/devices/2/commands/send?commandId="
-
-cmdzero 	= "3"
-cmdone 		= "4"
-cmdtwo 		= "5"
-cmdthree 	= "6"
-cmdfour 	= "7"
-cmdfive 	= "8"
-cmdsix 		= "9"
-cmdseven 	= "10"
-cmdeight 	= "11"
-cmdnine 	= "12"
-cmdchup		= "18"
-cmdchdn 	= "19"
-cmdenter 	= "14"
-cmdinfo 	= "13"
-codlang 	= "21"
-cmdlast 	= "15"
-cmdmute 	= "17"
-cmdvolup 	= "16"
-cmdvoldn 	= "17"
 
 
-
-listen_for(/CNBC/i) {cnbc}
+listen_for(/NBC/i) {nbc}
+listen_for(/CBS/i) {cbs}
+listen_for(/ABC/i) {abc}
+listen_for(/FOX/i) {fox}
 listen_for(/ESPN/i) {espn}
+listen_for(/CNBC/i) {cnbc}
 listen_for(/weather channel/i) {twc}
-listen_for(/nickelodeon/i) {nick}
 
 
-  def cnbc
-    say "OK. Changing the channel to CNBC."
-    Rest.get("#{reurl2}#{cmdfour}")
-    Rest.get("#{reurl2}#{cmdthree}")
-    Rest.get("#{reurl2}#{cmdenter}")
+  def nbc
+    say "OK. Changing the channel to NBC."
+    Rest.get("#{@reUrl2}#{@cmdThree}")
+    sleep(1)
+    Rest.get("#{@reUrl2}#{@cmdEnter}")
+    request_completed
+  end
+
+  def cbs
+    say "OK. Changing the channel to CBS."
+    Rest.get("#{@reUrl2}#{@cmdNine}")
+    sleep(1)
+    Rest.get("#{@reUrl2}#{@cmdEnter}")
+    request_completed
+  end
+
+  def abc
+    say "OK. Changing the channel to ABC."
+    Rest.get("#{@reUrl2}#{@cmdOne}")
+    sleep(1)
+    Rest.get("#{@reUrl2}#{@cmdZero}")
+    sleep(1)
+    Rest.get("#{@reUrl2}#{@cmdEnter}")
+    request_completed
+  end
+
+  def fox
+    say "OK. Changing the channel to FOX."
+    Rest.get("#{@reUrl2}#{@cmdOne}")
+    sleep(1)
+    Rest.get("#{@reUrl2}#{@cmdOne}")
+    sleep(1)
+    Rest.get("#{@reUrl2}#{@cmdEnter}")
     request_completed
   end
 
   def espn
     say "OK. Changing the channel to ESPN."
-    Rest.get("#{reurl2}#{cmdthree}")
-    Rest.get("#{reurl2}#{cmdthree}")
-    Rest.get("#{reurl2}#{cmdenter}")
+    Rest.get("#{@reUrl2}#{@cmdThree}")
+    sleep(1)
+    Rest.get("#{@reUrl2}#{@cmdThree}")
+    sleep(1)
+    Rest.get("#{@reUrl2}#{@cmdEnter}")
+    request_completed
+  end
+
+  def cnbc
+    say "OK. Changing the channel to CNBC."
+    Rest.get("#{@reUrl2}#{@cmdFour}")
+    sleep(1)
+    Rest.get("#{@reUrl2}#{@cmdThree}")
+    sleep(1)
+    Rest.get("#{@reUrl2}#{@cmdEnter}")
     request_completed
   end
 
   def twc
     say "OK. Changing the channel to The Weather Channel."
-    Rest.get("#{reurl2}#{cmdfive}")
-    Rest.get("#{reurl2}#{cmdeight}")
-    Rest.get("#{reurl2}#{cmdenter}")
-    request_completed
-  end
-
-  def nick
-    say "OK. Changing the channel to Nickelodeon."
-    Rest.get("#{reurl2}#{cmdsix}")
-    Rest.get("#{reurl2}#{cmdone}")
-    Rest.get("#{reurl2}#{cmdenter}")
+    Rest.get("#{@reUrl2}#{@cmdFive}")
+    sleep(1)
+    Rest.get("#{@reUrl2}#{@cmdEight}")
+    sleep(1)
+    Rest.get("#{@reUrl2}#{@cmdEnter}")
     request_completed
   end
 
