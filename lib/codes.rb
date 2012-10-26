cc41ec13315bbac63ba8860e6319742d31d3dd58
elvisimprsntr <casual@alecdawson.com>
2012-10-26 05:25:40 -0300

    Update lib/codes.rb

---
+++ lib/codes.rb
@@ -0,0 +1,22 @@
+
+codes = Hash.new
+
+codes["0"] = 3
+codes["1"] = 4
+codes["2"] = 5
+codes["3"] = 6
+codes["4"] = 7
+codes["5"] = 8
+codes["6"] = 9
+codes["7"] = 10
+codes["8"] = 11
+codes["9"] = 12
+codes["chup"] = 18
+codes["chdn"] = 19
+codes["enter"] = 14
+codes["info"] = 13
+codes["lang"] = 21
+codes["last"] = 15
+codes["mute"] = 17
+codes["volup"] = 16
+codes["voldn"] = 17
