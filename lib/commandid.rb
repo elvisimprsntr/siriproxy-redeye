a21b905c588a7250d2d83569e5c49dabcec20593
elvisimprsntr <casual@alecdawson.com>
2012-10-26 13:59:19 -0300

    Update lib/commandid.rb

--- lib/commandid.rb
+++ lib/commandid.rb
@@ -1,27 +1,35 @@
 require 'cora'
 
-def commandID(init)
+def commandID
 
-cmdid = Hash.new
+@cmdId = Hash.new
+@cmdId["0"] = 3
+@cmdId["1"] = 4
+@cmdId["2"] = 5
+@cmdId["3"] = 6
+@cmdId["4"] = 7
+@cmdId["5"] = 8
+@cmdId["6"] = 9
+@cmdId["7"] = 10
+@cmdId["8"] = 11
+@cmdId["9"] = 12
+@cmdId["chup"] = 18
+@cmdId["chdn"] = 19
+@cmdId["enter"] = 14
+@cmdId["info"] = 13
+@cmdId["lang"] = 21
+@cmdId["last"] = 15
+@cmdId["mute"] = 20
+@cmdId["volup"] = 16
+@cmdId["voldn"] = 17
 
-cmdid["0"] = 3
-cmdid["1"] = 4
-cmdid["2"] = 5
-cmdid["3"] = 6
-cmdid["4"] = 7
-cmdid["5"] = 8
-cmdid["6"] = 9
-cmdid["7"] = 10
-cmdid["8"] = 11
-cmdid["9"] = 12
-cmdid["chup"] = 18
-cmdid["chdn"] = 19
-cmdid["enter"] = 14
-cmdid["info"] = 13
-cmdid["lang"] = 21
-cmdid["last"] = 15
-cmdid["mute"] = 20
-cmdid["volup"] = 16
-cmdid["voldn"] = 17
+@stationId = Hash.new
+@stationId["nbc"] = 3
+@stationId["cbs"] = 9
+@stationId["abc"] = 10
+@stationId["fox"] = 11
+@stationId["espn"] = 22
+@stationId["cnbc"] = 43
+@stationId["weather channel"] = 58
 
 end
