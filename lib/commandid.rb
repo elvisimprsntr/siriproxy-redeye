b5fbca7e303b083ce13ae62c03c1e06c22a52ffa
elvisimprsntr <casual@alecdawson.com>
2012-10-27 07:19:45 -0300

    Update lib/commandid.rb

--- lib/commandid.rb
+++ lib/commandid.rb
@@ -2,41 +2,6 @@ require 'cora'
 
 def commandID
 
-@cmdId = Hash.new
-@cmdId["0"] = 3
-@cmdId["1"] = 4
-@cmdId["2"] = 5
-@cmdId["3"] = 6
-@cmdId["4"] = 7
-@cmdId["5"] = 8
-@cmdId["6"] = 9
-@cmdId["7"] = 10
-@cmdId["8"] = 11
-@cmdId["9"] = 12
-@cmdId["chup"] = 18
-@cmdId["chdn"] = 19
-@cmdId["enter"] = 14
-@cmdId["info"] = 13
-@cmdId["lang"] = 21
-@cmdId["last"] = 15
-@cmdId["mute"] = 20
-@cmdId["volup"] = 16
-@cmdId["voldn"] = 17
-
-@stationId = Hash.new
-@stationId["nbc"] = 3
-@stationId["cbs"] = 9
-@stationId["abc"] = 10
-@stationId["fox"] = 11
-@stationId["css"] = 32
-@stationId["espn"] = 33
-@stationId["espn2"] = 34
-@stationId["sun sports"] = 35
-@stationId["golf channel"] = 36
-@stationId["fox sports"] = 37
-@stationId["cnbc"] = 43
-@stationId["foxnews"] = 44
-@stationId["fox news"] = 44
-@stationId["weather channel"] = 58
+# Reserved for future use
 
 end
