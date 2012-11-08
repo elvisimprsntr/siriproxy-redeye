06046a02c3dd99c257c15d27f61407264ee22916
Elvis <casual@alecdawson.com>
2012-11-08 08:58:07 -0500

    fixed empty hash match case

--- lib/siriproxy-redeye.rb
+++ lib/siriproxy-redeye.rb
@@ -55,11 +55,9 @@ class SiriProxy::Plugin::RedEye < SiriProxy::Plugin
   end
 
   def send_command(command)
-	commandid = @cmdId[@reRoom][@reDevice][command.downcase.strip]
-	unless commandid.nil?
+	commandid = @cmdId[@reRoom][@reDevice][command.downcase.strip] rescue nil
+	unless commandid.empty?
 		say "OK. Sending command #{command}."
-# FIXIT: Does not properly handle no match.  Results in "can't convert Hash into String (TypeError)"
-# 	This may be due to the fact that dynamically created multidimensional hash will create new keys if a match is not found which will pass the NIL check.
 		Rest.get(@reIp[@reSel] + @roomId[@reRoom] + @deviceId[@reRoom][@reDevice] + commandid)
 	else
 		say "Sorry, I am not programmed for command #{command}."
